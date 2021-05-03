0450989b9a708ee95dc64b44f2712cf9db353ad0
Mari Sinkkonen <amiliini@hotmail.com>
2021-05-03 14:06:48 +0300

    kurssitiedot step1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+const App = () => {
+  const kurssitiedot = {
+    nimi: "Half Stack application development",
+    osat: [
+      {
+        title: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        title: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        title: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header kurssitiedot={kurssitiedot} />
+      <Content kurssitiedot={kurssitiedot} />
+      <Total kurssitiedot={kurssitiedot} />
+      {/* <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
+    </div>
+  );
+};
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.kurssitiedot.nimi}</h1>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.kurssitiedot.osat[0].title}{" "}
+        {props.kurssitiedot.osat[0].exercises}
+      </p>{" "}
+      <p>
+        {props.kurssitiedot.osat[1].title}{" "}
+        {props.kurssitiedot.osat[1].exercises}
+      </p>
+      <p>
+        {props.kurssitiedot.osat[2].title}{" "}
+        {props.kurssitiedot.osat[2].exercises}
+      </p>
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      {props.kurssitiedot.osat[0].exercises +
+        props.kurssitiedot.osat[1].exercises +
+        props.kurssitiedot.osat[2].exercises}
+    </p>
+  );
+};
+
+export default App;
