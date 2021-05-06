b42fcb47969996931ccf4242081e4071e14c8cc6
Mari Sinkkonen <amiliini@hotmail.com>
2021-05-06 13:44:52 +0300

    step5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,65 +1,56 @@
 import React from "react";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   const Header = () => {
+    console.log(course.parts[0].name);
     return (
       <>
-        <h1>{course}</h1>
+        <h1>{course.name}</h1>
       </>
     );
   };
   const Content = () => {
-    console.log(parts);
+    console.log(course.parts);
     return (
       <>
         <p>
-          {parts[0].name} {parts[0].exercises}
+          {course.parts[0].name} {course.parts[0].exercises}
         </p>
         <p>
-          {parts[1].name} {parts[1].exercises}
+          {course.parts[1].name} {course.parts[1].exercises}
         </p>
         <p>
-          {parts[2].name} {parts[2].exercises}
+          {course.parts[2].name} {course.parts[2].exercises}
         </p>
-
-        {/* <Part name={parts[0].name} exercises={parts[0].exercises} />
-        <Part name={parts[1].name} exercises={parts[1].exercises} />
-        <Part name={parts[2].name} exercises={parts[2].exercises} /> */}
       </>
     );
   };
 
-  // const Part = () => {
-  //   return (
-  //     <>
-  //       <p>
-  //         {parts.name} {parts.exercises}
-  //       </p>
-  //     </>
-  //   );
-  // };
   const Total = () => {
-    console.log(parts);
     return (
       <p>
         Number of exercises{" "}
-        {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+        {course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises}
       </p>
     );
   };
@@ -67,8 +58,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course} />
+      <Total parts={course} />
     </div>
   );
 };
